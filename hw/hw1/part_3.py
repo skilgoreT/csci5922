9506eefe51c51b3c9221607d7d66f81c7bb5d359
Sean Kelly <sean.kelly@simbulus.com>
2017-09-11 13:43:33 -0600

    hw1 notebook has the definative version of the code

--- hw/hw1/part_3.py
+++ hw/hw1/part_3.py
@@ -3,14 +3,18 @@ import numpy as np
 import scipy.io
 import scipy.optimize as optimization
 
-EPS = 0.001
-N_EPOCH = 1000
-def dw(w,d,X):
+N_EPOCH = 30
+def dwp(w,d,X):
     # using numpy einstein summation to vectorize the computation
-    # vector of coefficients (d-w.x) for each pattern (alpha)
-    C = d - np.einsum('i,ij->j', w, X) # shape is (100,)
-    # C_i * X_ji or C * transpose(X)
-    dw = EPS*np.einsum('i,ji->j', C, X) # shape is (3,)
+    # calculating w.x.d -- w.x.d < 0 => incorrect classification
+    C = d*np.einsum('i,ij->j', w, X) # shape is (100,)
+    XT = X.transpose()
+    # use enumerate for the equivalent to each_with_index (Ruby)
+    xd = [ d[i]*XT[i] for i,c in enumerate(C) if c < 0]
+    print(len(xd))
+    # sum xd element-wise
+    # dw is x.t summed over misclassified teachers
+    dw = np.einsum('ij->j',xd) # shape is (3,)
     return dw
 
 def main():
@@ -22,13 +26,16 @@ def main():
     x2 = data['x'][:,1]
     # inputs - with bias input tied high, shape is (3,100)
     X = np.array([np.full(len(x1),1),x1,x2])
+    XT = X.transpose();
     # output
-    d = np.array(data['y']).flatten()
+    d = np.array(data['z']).flatten()
+    # use domain -1,1 for teachers so we can use x.w.d<0 for the classification test
+    d = map(lambda x: -1 if x==0 else x, d)
     # starting vector of weights
     w = [1,1,1]
     # sweep through the data N_EPOCH times
     for _ in range(N_EPOCH):
-        w = w + dw(w,d,X)
+        w = w + dwp(w,d,X)
     print(w)
 
 # invoke main
